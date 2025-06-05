dfb0925e17d5a4ab9cbfcca54eadabedd6e5eaee
Amon20044 <202251015@iiitvadodara.ac.in>
2025-06-05 18:30:06 +0530

    nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,7 +30,7 @@ export default function Navbar() {
 
   return (
     <header className="bg-white font-[Raleway] shadow-lg sticky top-0 z-50">
-      <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <nav className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
           <div className="flex-shrink-0">
