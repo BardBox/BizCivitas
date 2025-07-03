bec4e62d30e26b54d61e998e3d4150f3e1748962
Amon20044 <202251015@iiitvadodara.ac.in>
2025-07-03 12:19:34 +0530

    footer size fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -161,7 +161,7 @@ export default function Footer() {
         style={{ backgroundColor: bgColor }}
         aria-label="Site Footer"
       >
-        <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <div className="max-w-[1440px] mx-auto px-4 sm:px-6 lg:px-8 py-12">
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-start">
             {/* Company Info */}
             <section className="lg:col-span-1" aria-labelledby="company-info">
